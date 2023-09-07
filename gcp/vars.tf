df0b92ecc575548727ab046b2539486671f70fba
Gautam <gautambaghel93@gmail.com>
2023-09-07 16:29:06 -0700

    enabling all the relevant GCP services and granting admin roles to SA

--- gcp/vars.tf
+++ gcp/vars.tf
@@ -33,10 +33,28 @@ variable "gcp_service_list" {
   description = "APIs required for the project"
   type        = list(string)
   default = [
-    "iam.googleapis.com",
+    "apigee.googleapis.com",
+    "artifactregistry.googleapis.com",
+    "autoscaling.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "cloudfunctions.googleapis.com",
     "cloudresourcemanager.googleapis.com",
+    "compute.googleapis.com",
+    "container.googleapis.com",
+    "containerfilesystem.googleapis.com",
+    "containerregistry.googleapis.com",
+    "deploymentmanager.googleapis.com",
+    "dns.googleapis.com",
+    "iam.googleapis.com",
+    "iamcredentials.googleapis.com",
+    "networkconnectivity.googleapis.com",
     "sts.googleapis.com",
-    "iamcredentials.googleapis.com"
+    "serviceusage.googleapis.com",
+    "storage-api.googleapis.com",
+    "storage-component.googleapis.com",
+    "storage.googleapis.com",
+    "sts.googleapis.com",
+    "vpcaccess.googleapis.com"
   ]
 }
 
@@ -44,6 +62,33 @@ variable "gcp_roles_list" {
   description = "Roles required for the Service Account"
   type        = list(string)
   default = [
-    "roles/storage.admin"
+    "roles/apigee.admin",
+    "roles/apigee.apiAdminV2",
+    "roles/appengine.appAdmin",
+    "roles/clouddeploy.admin",
+    "roles/cloudfunctions.admin",
+    "roles/cloudkms.admin",
+    "roles/compute.admin",
+    "roles/compute.instanceAdmin",
+    "roles/compute.instanceAdmin.v1",
+    "roles/compute.storageAdmin",
+    "roles/config.admin",
+    "roles/container.admin",
+    "roles/container.clusterAdmin",
+    "roles/dns.admin",
+    "roles/gkemulticloud.admin",
+    "roles/iam.roleAdmin",
+    "roles/iam.securityAdmin",
+    "roles/iam.serviceAccountAdmin",
+    "roles/iam.serviceAccountKeyAdmin",
+    "roles/iam.serviceAccountTokenCreator",
+    "roles/resourcemanager.projectIamAdmin",
+    "roles/run.admin",
+    "roles/secretmanager.admin",
+    "roles/servicenetworking.networksAdmin",
+    "roles/source.admin",
+    "roles/storage.admin",
+    "roles/storage.objectAdmin",
+    "roles/vpcaccess.admin"
   ]
 }
